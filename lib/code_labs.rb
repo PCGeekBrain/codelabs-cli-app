2aa2cb0ec7ce2789eb82af8fc9f8df2b044870ac
Mendel Hornbacher <me@mendel.tech>
2017-05-23 13:08:36 -0400

    comment

--- lib/code_labs.rb
+++ lib/code_labs.rb
@@ -1,5 +1,5 @@
 require_relative "./code_labs/version"
-
+# include all the files in the namespace
 require_relative "./code_labs/scraper"
 require_relative "./code_labs/lab"
 require_relative "./code_labs/tech"
