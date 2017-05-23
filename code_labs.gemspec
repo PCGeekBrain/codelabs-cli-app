03472c30affb1f373dd791194338c424d8068921
Mendel Hornbacher <me@mendel.tech>
2017-05-23 13:05:23 -0400

    removed bad dependecy (+ more comments)

--- code_labs.gemspec
+++ code_labs.gemspec
@@ -35,5 +35,4 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rspec", "~> 3.0"
   spec.add_development_dependency "pry", "~> 0.10"
   spec.add_dependency "nokogiri"
-  spec.add_dependency "open-uri"
 end
--- lib/code_labs/scraper.rb
+++ lib/code_labs/scraper.rb
@@ -1,28 +1,30 @@
 require 'openssl'
 require 'open-uri'
 require 'nokogiri'
-require 'pry'
 
 class CodeLabs::Scraper
     def initialize(url='https://codelabs.developers.google.com')
         @url = url
     end
     def scrape
-        puts "Loading data...."
-        doc = Nokogiri::HTML(open(@url, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}))
+        puts "Loading data...." # <- show the user what is happening
+        doc = Nokogiri::HTML(open(@url, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE})) # no SSL, SSL is breaking this
         puts "Parsing data...."
+        # each of the ~250 cards on the page
         doc.css('#cards a').each do |card|
+            # Create a new lab
             lab = CodeLabs::Lab.new({
-                title: card.css('.description').text.split.join(" "), 
-                duration: card.css('.card-duration span').text,
-                link: @url + card['href'].split("?")[0], 
-                author: card.css('.card-author').text,
-                last_updated: card.css('.card-updated').text[8..-1].strip
+                title: card.css('.description').text.split.join(" "), # <- there are no titles and these are short enough
+                duration: card.css('.card-duration span').text, # is already nice and formatted
+                link: @url + card['href'].split("?")[0], # <- the link they can go to
+                author: card.css('.card-author').text, # <- the author if he exists
+                last_updated: card.css('.card-updated').text[8..-1].strip # <- remove the 'Updated' part
             })
+            # now the tech is buried in a coustom polymer attribute, so....
             techs = card.get_attribute('data-category').split(",").each do |tech_name|
+                # use find or create to get the tech object by name and add the lab to it
                 CodeLabs::Tech.find_or_create(tech_name).add_lab(lab)
-            end
-        end
-        # binding.pry
-    end
-end
+            end # <- end tech items
+        end # <- end cards
+    end # <- end scrape
+end # <- end Scraper
