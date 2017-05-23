52fe0bfd56df4d8e1d3d22ea568375f63f88fe87
Mendel Hornbacher <me@mendel.tech>
2017-05-23 12:58:03 -0400

    added comments

--- lib/code_labs/tech.rb
+++ lib/code_labs/tech.rb
@@ -1,9 +1,9 @@
 # require 'pry'
 class CodeLabs::Tech
-    @@all = []
-    attr_reader :name
+    @@all = [] # <- The manager
+    attr_reader :name # <- for display, saves 2 lines
 
-    def initialize(name) # All tech has a name
+    def initialize(name) # All tech has a name, no?
         @name = name
         @labs = []
     end
@@ -12,24 +12,24 @@ class CodeLabs::Tech
         self # return the object not the list
     end
     def labs
-        @labs.dup.freeze
+        @labs.dup.freeze # <- no type editing here
     end
-    def add_lab(lab)
+    def add_lab(lab) # <- type enforcment
         raise TypeError unless lab.is_a?(CodeLabs::Lab)
         @labs << lab
-        lab.add_tech(self) unless lab.techs.include?(self)
+        lab.add_tech(self) unless lab.techs.include?(self) # <- infinite loop
     end
 
     def self.all
-        @@all
+        @@all # <- the core of the cli listing is in here
     end
     def self.clear_all
-        @@all.clear
+        @@all.clear # <- I do not think I ended up needing this
     end
     def self.create(name)
-        self.new(name).save
+        self.new(name).save # <- fancy
     end
-    def self.find_or_create(name)
+    def self.find_or_create(name) # <- the meat of the creation
         res = @@all.detect{|tech| tech.name == name}
         res ? res : self.create(name)
     end
