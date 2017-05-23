398125fcf9f0fd773d052026e5c660ad0cdf40b1
Mendel Hornbacher <me@mendel.tech>
2017-05-23 10:30:41 -0400

    init working

--- lib/code_labs/lab.rb
+++ lib/code_labs/lab.rb
@@ -1,3 +1,12 @@
+
 class CodeLabs::Lab
-    attr_accessor :title, :duration, :link, :autor, :last_updated
+    attr_accessor :title, :duration, :link, :author, :last_updated
+
+    def initialize(arguments={})
+        # default values
+        @duration = "N/A"
+        @author = "N/A"
+        # Mass assignment
+        arguments.each {|key, value| self.send("#{key}=", value)}
+    end
 end
--- spec/lab_spec.rb
+++ spec/lab_spec.rb
@@ -1,6 +1,6 @@
 RSpec::Expectations.configuration.on_potential_false_positives = :nothing
 
-RSpec.describe "Tech" do
+RSpec.describe "Lab" do
     context "#initialize" do
         let(:lab1) {CodeLabs::Lab.new({title: 'Title', duration:"15 min", link: "google.com", author: "bob", last_updated: "May 22, 2017"})}
         it "creates a valid object" do
@@ -11,11 +11,10 @@ RSpec.describe "Tech" do
             expect(lab1.author).to eq("bob")
             expect(lab1.last_updated).to eq("May 22, 2017")
         end
-        it "supplies default values for some fields" do
+        it "supplies default values for duration and author" do
             @lab = CodeLabs::Lab.new
-            expect(lab1.duration).to eq('N/A')
-            expect(lab1.author).to eq('N/A')
-            expect(lab1.last_updated).to eq('N/A')
+            expect(@lab.duration).to eq('N/A')
+            expect(@lab.author).to eq('N/A')
         end
     end
 end
